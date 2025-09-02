dfa471ac52e50d05a6b776538f590ef59e901834
EJtaro <ejcoms.taro@gmail.com>
2025-09-02 09:37:03 +0900

    ・ping用エンドポイント追加 ・ログ出力コメント追加

--- app/routes/main.py
+++ app/routes/main.py
@@ -7,12 +7,7 @@ bp = Blueprint('main', __name__)  # 'main' は Blueprint の名前
 def root():
     return redirect(url_for("auth.login"))
 
-# @bp.route("/index", methods=["GET", "POST"])
-# def index():
-#     keyword = request.args.get("keyword")  # GETの値（例：検索）
-#     name = request.form.get("username") if request.method == "POST" else None  # POSTの値（例：フォーム入力）
-#     return render_template("index.html", keyword=keyword, name=name) # テンプレート名と変数
-
-# 訓練通知画面
-# @bp.route("/training",methods=["GET"])
-
+# 稼働チェック(UptimeRobot)用エンドポイント
+@bp.route("/ping", methods=["GET", "HEAD"])
+def ping_check():
+    return "", 200
--- app/services/log.py
+++ app/services/log.py
@@ -5,7 +5,9 @@ import sentry_sdk
 logger = logging.getLogger("app_logger")
 logger.setLevel(logging.INFO)
 
+# ログ出力先のハンドラー設定
 if not logger.hasHandlers():
+    # コンソールにログを出すStreamHandlerを設定
     handler = logging.StreamHandler()
     formatter = logging.Formatter("[%(asctime)s] %(levelname)s: %(message)s")
     handler.setFormatter(formatter)
@@ -14,9 +16,11 @@ if not logger.hasHandlers():
 # エラー発生時に一括でロギング & Sentry送信
 # 呼び出し例:log_error("エラーメッセージ", 例外[省略可], ユーザーID[省略可], 辞書型のその他情報[省略可])
 def log_error(message: str, exception: Exception = None, user_id=None, extra: dict = None):
+    # ローカルにエラーログ出力
     error_msg = f"{message}: {str(exception)}" if exception else message
     logger.error(error_msg)
 
+    # Sentryにログの内容を送信
     with sentry_sdk.push_scope() as scope:
         # 追加情報のセット
         if user_id: # ユーザーID
@@ -26,6 +30,8 @@ def log_error(message: str, exception: Exception = None, user_id=None, extra: di
                 scope.set_extra(k, v)
         # Sentryへの送信
         if exception:
+            # 例外ログ
             sentry_sdk.capture_exception(exception)
         else:
+            # Errorレベルとして扱われる例外なしのログ
             sentry_sdk.capture_message(message, level="error")
